8e49f0546d6f079dd00caaa60d8c401c7eae40ea
mahakporwal02 <mahakmanishporwal@gmail.com>
2021-08-05 03:30:03 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.svg">
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg">
+    <img alt="img-heading" class="img-heading" src="./assets/heading.svg">
+    <img alt="heading-pink" class="heading-pink" src ="./assets/heading pink.svg">
+    <img alt="white curve" class="white-curve" src ="./assets/white curve.svg">
+    <img alt="pink curve" class="pink-curve" src ="./assets/pink curve.svg">
+    <img alt="grey curve" class="grey-curve" src ="./assets/grey curve.svg">
+    <img alt="nav rect" class="nav-rect" src ="./assets/nav rect.svg">
+    <img alt="auth rect" class="auth-rect" src ="./assets/auth rect.svg">
+    <img alt="black curve" class="black-curve" src ="./assets/black curve.svg">
+    <img alt="black rect" class="black-rect" src ="./assets/black_rect.svg">
+    <!--<img alt="black_bottom_curve" class="black-bcurve" src ="./assets/black_bottom_curve.svg">-->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,12 +27,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
