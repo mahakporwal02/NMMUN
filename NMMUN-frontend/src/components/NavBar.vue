758e4e9a4ebc11780331f7dd0bf46c1ade0abc38
mahakporwal02 <mahakmanishporwal@gmail.com>
2021-09-03 02:19:15 +0530

    made navbar resposnive

--- NMMUN-frontend/src/components/NavBar.vue
+++ NMMUN-frontend/src/components/NavBar.vue
@@ -1,21 +1,79 @@
 <template>
-  <div>
-    <img alt="Vue logo" class="logo" src="../assets/logo.svg">
-    <div class="main-navbar">
-    <ul class="main-nav-ul">
-        <li><a href="#">ABOUT</a></li>
-        <li><a href="#">SECRETARIAT</a></li>
-        <li><a href="#">COMMITTEES</a></li>
-        <li><a href="#">GALLERY</a></li>
-        <li><a href="#">RESOURCES</a></li>
-    </ul>
+  <div class="container-fluid">
+    <div class="display">
+      <div class="row ml-4">
+        <div class="col-lg-2 logo">
+          <img alt="Vue logo" src="../assets/logo.svg" />
+        </div>
+        <div class="main-nav col-lg-6 col-xl-5 mt-4 offset-lg-1">
+          <div class="row">
+            <div class="col-2 neg-margin">
+              <a class="ml-4" style="color: #DDB771; text-decoration: none;"  href="#">ABOUT</a>
+            </div>
+            <div class="col-2">
+              <a class="ml-2" style="color: #DDB771; text-decoration: none;" href="#/sec">SECRETARIAT</a>
+            </div>
+            <div class="col-3" style="text-align:center;">
+              <a class="ml-4 pl-2" style="color: #DDB771; text-decoration: none;" href="#/committees">COMMITTEES</a>
+            </div>
+            <div class="col-2">
+              <a class="ml-2 p-lg-2" style="color: #DDB771; text-decoration: none;" href="#/gallery">GALLERY</a>
+            </div>
+            <div class="col-2">
+              <a class="ml-2" style="color: #DDB771; text-decoration: none;" href="#/resources">RESOURCES</a>
+            </div>
+          </div>
+        </div>
+        <div
+          class="side-nav col-lg-1 offset-xl-2 offset-lg-1  mt-4"
+          style="text-align:center;"
+        >
+          <a class="neg-mg-side-nav" style="color: #DDB771; text-decoration: none;" href="#">REGISTER</a>
+        </div>
+      </div>
     </div>
-    <div class="side-navbar">REGISTER</div>
+
+    <div class="display-small">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <a href="#"><img src="../assets/logo.svg"></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#nav-toggle"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-toggle">
+          <ul class="navbar-nav mr-auto li-color">
+            <li class="nav-item active">
+              <a class="nav-link" style="color: #dbaf5d;" href="#/about">ABOUT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="color: #dbaf5d;" href="#/sec">SECRETARIAT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="color: #dbaf5d;" href="#/committees">
+                COMMITTEES
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " style="color: #dbaf5d;" href="#/gallery">GALLERY</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " style="color: #dbaf5d;" href="#/resources">RESOURCES</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'NavBar',
+  name: "NavBar",
 };
 </script>
+<style scoped></style>
