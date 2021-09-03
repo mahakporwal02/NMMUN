c07850562ae0bbecb58b27c5321d4308435c4280
mahakporwal02 <mahakmanishporwal@gmail.com>
2021-09-04 02:50:33 +0530

    made homepage for mobile view and desktop vie

--- NMMUN-frontend/src/components/NavBar.vue
+++ NMMUN-frontend/src/components/NavBar.vue
@@ -3,24 +3,49 @@
     <div class="display">
       <div class="row ml-4">
         <div class="col-lg-2 logo">
-          <img alt="Vue logo" src="../assets/logo.svg" />
+          <img class="logo" alt="Vue logo" src="../assets/logo.svg" />
         </div>
         <div class="main-nav col-lg-6 col-xl-5 mt-4 offset-lg-1">
           <div class="row">
             <div class="col-2 neg-margin">
-              <a class="ml-4" style="color: #DDB771; text-decoration: none;"  href="#">ABOUT</a>
+              <a
+                class="ml-4"
+                style="color: #DDB771; text-decoration: none;"
+                href="#"
+                >ABOUT</a
+              >
             </div>
             <div class="col-2">
-              <a class="ml-2" style="color: #DDB771; text-decoration: none;" href="#/sec">SECRETARIAT</a>
+              <a
+                class="ml-2"
+                style="color: #DDB771; text-decoration: none;"
+                href="#/sec"
+                >SECRETARIAT</a
+              >
             </div>
             <div class="col-3" style="text-align:center;">
-              <a class="ml-4 pl-2" style="color: #DDB771; text-decoration: none;" href="#/committees">COMMITTEES</a>
+              <a
+                class="ml-4 pl-2"
+                style="color: #DDB771; text-decoration: none;"
+                href="#/committees"
+                >COMMITTEES</a
+              >
             </div>
             <div class="col-2">
-              <a class="ml-2 p-lg-2" style="color: #DDB771; text-decoration: none;" href="#/gallery">GALLERY</a>
+              <a
+                class="ml-2 p-lg-2"
+                style="color: #DDB771; text-decoration: none;"
+                href="#/gallery"
+                >GALLERY</a
+              >
             </div>
             <div class="col-2">
-              <a class="ml-2" style="color: #DDB771; text-decoration: none;" href="#/resources">RESOURCES</a>
+              <a
+                class="ml-2"
+                style="color: #DDB771; text-decoration: none;"
+                href="#/resources"
+                >RESOURCES</a
+              >
             </div>
           </div>
         </div>
@@ -28,14 +53,19 @@
           class="side-nav col-lg-1 offset-xl-2 offset-lg-1  mt-4"
           style="text-align:center;"
         >
-          <a class="neg-mg-side-nav" style="color: #DDB771; text-decoration: none;" href="#">REGISTER</a>
+          <a
+            class="neg-mg-side-nav"
+            style="color: #DDB771; text-decoration: none;"
+            href="#/register"
+            >REGISTER</a
+          >
         </div>
       </div>
     </div>
 
     <div class="display-small">
       <nav class="navbar navbar-expand-lg navbar-light">
-        <a href="#"><img src="../assets/logo.svg"></a>
+        <a href="#"><img class="logo" src="../assets/logo.svg"/></a>
         <button
           class="navbar-toggler"
           type="button"
@@ -48,10 +78,14 @@
         <div class="collapse navbar-collapse" id="nav-toggle">
           <ul class="navbar-nav mr-auto li-color">
             <li class="nav-item active">
-              <a class="nav-link" style="color: #dbaf5d;" href="#/about">ABOUT</a>
+              <a class="nav-link" style="color: #dbaf5d;" href="#/about"
+                >ABOUT</a
+              >
             </li>
             <li class="nav-item">
-              <a class="nav-link" style="color: #dbaf5d;" href="#/sec">SECRETARIAT</a>
+              <a class="nav-link" style="color: #dbaf5d;" href="#/sec"
+                >SECRETARIAT</a
+              >
             </li>
             <li class="nav-item">
               <a class="nav-link" style="color: #dbaf5d;" href="#/committees">
@@ -59,14 +93,31 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link " style="color: #dbaf5d;" href="#/gallery">GALLERY</a>
+              <a class="nav-link " style="color: #dbaf5d;" href="#/gallery"
+                >GALLERY</a
+              >
             </li>
             <li class="nav-item">
-              <a class="nav-link " style="color: #dbaf5d;" href="#/resources">RESOURCES</a>
+              <a class="nav-link " style="color: #dbaf5d;" href="#/resources"
+                >RESOURCES</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " style="color: #dbaf5d;" href="#/register"
+                >REGISTER</a
+              >
             </li>
           </ul>
         </div>
       </nav>
+    </div>
+    <div class="row">
+      <div class="col-lg-5 ">
+        <img class="heading" src="../assets/header.svg"/>
+      </div>
+       <div class="col-lg-3">
+        <img class="side_heading" src="../assets/side_header.svg"/>
+      </div>
     </div>
   </div>
 </template>
