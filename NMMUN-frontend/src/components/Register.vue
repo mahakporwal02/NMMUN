65668ffd7dfcc1a0bd6408a4f8f26a0aadf7222c
mahakporwal02 <mahakmanishporwal@gmail.com>
2021-09-10 01:47:39 +0530

    added register page

--- NMMUN-frontend/src/components/Register.vue
+++ NMMUN-frontend/src/components/Register.vue
@@ -1,6 +1,50 @@
 <template>
   <div>
-      nnnnnnnnnn
+    <form class="offset-sm-1">
+      <div class="form-group">
+        <label for="exampleInputEmail1">First Name</label>
+        <input
+          type="email"
+          class="form-control col-sm-5"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Enter First Name"
+        />
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Last Name</label>
+        <input
+          type="email"
+          class="form-control col-sm-5"
+          placeholder="Enter Last Name"
+        />
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input
+          type="email"
+          class="form-control col-sm-5"
+          placeholder="Enter email"
+        />
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Committee</label>
+        <input
+          type="text"
+          class="form-control col-sm-5"
+          placeholder="Committee 1"
+        />
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Committee</label>
+        <input
+          type="text"
+          class="form-control col-sm-5"
+          placeholder="Committee 2"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
 </template>
 
